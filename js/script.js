e1cb1c3de98d4f19b4eb9b513f654aed287bd24f
avsoftmax <admin@avsoftmax.com>
2023-05-27 09:50:06 +0530

    Fixed bugs

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ function isMobileBrowser() {
 }
 
 // Set the date we're counting down to
-var countDownDate = new Date("Thu Jun 01 2023 10:00:00 GMT+0530").getTime();
+var countDownDate = new Date("Thu Jun 01 2023 11:00:00 GMT+0530").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
